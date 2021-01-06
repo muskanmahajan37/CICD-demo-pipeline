f583bda0565f97f557c9f20383cdd34906da732e
Divyansh Jain <42649226+Divyanshji@users.noreply.github.com>
2021-01-06 17:10:13 +0530

    Add files via upload

---
+++ test_cases.js
@@ -0,0 +1,11 @@
+var chai = require('chai')
+var expect = require('chai').expect
+
+var logic = require('../default_test')
+
+
+describe('CASE',function(){
+    it('Case should run' , function(){
+        expect(logic()).to.eql('hello there');
+    })
+})
