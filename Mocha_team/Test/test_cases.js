f2e5b02e9a6c5b8c9f074df4c424349955557e78
Divyansh Jain <42649226+Divyanshji@users.noreply.github.com>
2021-01-13 12:50:32 +0530

    Update test_cases.js

--- Mocha_team/Test/test_cases.js
+++ Mocha_team/Test/test_cases.js
@@ -11,4 +11,7 @@ describe('CASE',function(){
      it('2nd Case is running' , function(){
         expect(logic()).to.eql('hello there');
     })
+     it('3rd Case is running' , function(){
+        expect(logic()).to.eql('there');
+    })
 })
